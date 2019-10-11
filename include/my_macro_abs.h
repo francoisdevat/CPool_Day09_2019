ef583848854eb8f21d5837f903222f968bc94d5c
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-11 05:40:31 -0400

    add of lib and task 1

---
+++ include/my_macro_abs.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** deade
+** File description:
+** daeaddada
+*/
+
+#ifndef __MY_MACRO_ABS__
+#define __MY_MACRO_ABS__
+
+#defineABS(Value) ((Value < 0) ? (-Value) : (Value))
+
+#endif /*__MY_MACRO_ABS__*/
+
