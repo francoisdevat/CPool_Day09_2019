d97b70ce51c7fa767097eef303aa65ab19634545
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-11 08:37:40 -0400

    add_of_my_params_to_array.c

---
+++ my_params_to_array.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2019
+** header
+** File description:
+** deader
+*/
+
+struct info_param *my_params_to_array(int ac, char **av)
+{
+
+    int i  = 0;
+    struct info_param **table;
+    table = malloc(sizeof(struct info_param*) + 1);
+
+    if (table == 0){
+        return (0);
+    }
+
+    while (i < ac){
+        table[i] = malloc(sizeof(struct info_param));
+        table[i]->length = my_strlen(av[k]);
+        table[i]->str = av[k];
+        table[i]->copy = my_strcpy(av[k]);
+        table[i]->word_array = my_show__word_array(av[k]);
+        i = i + 1;
+    }
+    table[i] = 0;
+    return (*table);
+}
