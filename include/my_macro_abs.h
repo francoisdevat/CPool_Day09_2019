12cd0bbff9e8152245b646807b4654174143248a
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-11 08:24:42 -0400

    add of task 1 and 2

--- include/my_macro_abs.h
+++ include/my_macro_abs.h
@@ -5,10 +5,5 @@
 ** daeaddada
 */
 
-#ifndef __MY_MACRO_ABS__
-#define __MY_MACRO_ABS__
-
-#defineABS(Value) ((Value < 0) ? (-Value) : (Value))
-
-#endif /*__MY_MACRO_ABS__*/
+#define ABS(value) (value > 0 ? value : value * (- 1))
 
